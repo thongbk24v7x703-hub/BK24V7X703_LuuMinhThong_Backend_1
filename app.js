385de4ff29e46dab1ae54902251c763f82d8fc78
thongbk24v7x703-hub <thongbk24v7x703@dttx.ctu.edu.vn>
2026-06-12 18:10:13 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-
+const contactRuter = require("./app/routes/contact.route")
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/contacts", contactRuter);
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
